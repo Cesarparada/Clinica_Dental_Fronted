84c0e3d03b38e7dded57753e8bf0bb1e16e67560
César David Parada Castillo <cesard.0925@gmail.com>
2023-05-15 00:33:58 +0200

    lista de usuarios y tabla en vista admin añadida

---
+++ src/containers/index.js
@@ -0,0 +1,12 @@
+import About from "./about";
+import Admin from "./admin";
+import Contact from "./contact";
+import Home from "./home";
+import Login from "./login";
+import Register from "./register";
+import UserProfile from "./user-profile";
+import PageNotFound from "./page-not-found";
+import RegistroOdontologo from "./registro-odontologo";
+import Citas from "./citas/Citas";
+
+export { About, Admin, Home, Login, Register, UserProfile, PageNotFound, Contact, RegistroOdontologo, Citas};
