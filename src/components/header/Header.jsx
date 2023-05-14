5b9682891ba025226019dbf6bd0d4a0d76665849
César David Parada Castillo <cesard.0925@gmail.com>
2023-05-15 00:29:55 +0200

    nav header creado con bootstrap vita admin add

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import "./Header.scss";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { updateAuthStoreStateLogOut } from "../../features/authentication/updateAuthState";
+import {
+  MdPersonOutline,
+  MdOutlineLogout,
+  MdKeyboardArrowDown,
+  MdOutlineLogin,
+} from "react-icons/md";
+
+export default function Header() {
+  // HOOKS
+  const navigate = useNavigate();
+  const authState = useSelector((state) => state.auth);
+  const isLoggedIn = authState.isLoggedIn;
+  const { name, role } = authState.userInfo;
+  const isAdmin = role == "admin";
+
+  // HANDLERS
+  const handleLogout = () => {
+    updateAuthStoreStateLogOut();
+    navigate("/");
+  };
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="/">Clínica Dental</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/contact">Contact</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+            {isLoggedIn && !isAdmin && (
+                <Nav.Link href="/citas">Citas</Nav.Link>
+            )}
+            {isAdmin && (
+              <>
+                <NavDropdown title="Sala Admin" id="collasible-nav-dropdown">
+                  <NavDropdown.Item href="/admin">Admin</NavDropdown.Item>
+                  <NavDropdown.Item href="/registroOdontologo">
+                    Regitro Odontologo
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </>
+            )}
+          </Nav>
+          <Nav>
+            {!isLoggedIn && (
+              <>
+                <Nav.Link href="/login">
+                  <MdOutlineLogin />
+                  Login
+                </Nav.Link>
+                <Nav.Link href="/register">Register</Nav.Link>
+              </>
+            )}
+
+            {isLoggedIn && (
+              <>
+                <NavDropdown title={name} id="collasible-nav-dropdown">
+                  <NavDropdown.Item href="/profile">
+                    <MdPersonOutline className="icon" />
+                    Profile
+                  </NavDropdown.Item>
+                  <NavDropdown.Item onClick={handleLogout}>
+                    <MdOutlineLogout />
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
