33cd2fd685f58baf508137dac192ec3e38ff7a3d
César David Parada Castillo <cesard.0925@gmail.com>
2023-05-15 00:30:47 +0200

    util de formato de fecha, store realizado

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Header from "./header/";
+import UsersList from "./user-list";
+import TablePagination from "./table-pagination/TablePagination";
+import DataListTable from"./data-list-table/DatalistTable"
+
+export { Header, UsersList, TablePagination, DataListTable };
