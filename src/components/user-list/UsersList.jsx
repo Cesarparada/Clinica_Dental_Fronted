5dfdff99cbc09aeb2e554e8a6d32cb108b19c612
César David Parada Castillo <cesard.0925@gmail.com>
2023-05-19 19:02:05 +0200

    chore: descarte de file sin utilidad

--- src/components/user-list/UsersList.jsx
+++ src/components/user-list/UsersList.jsx
@@ -1,48 +1,49 @@
-import React from "react";
-import "./UsersList.scss";
-import { TablePagination } from "../../components";
-import { dateFormat } from "../../_utils/date";
+// import React from "react";
+// import "./UsersList.scss";
+// import { TablePagination } from "../../components";
+// import { dateFormat } from "../../_utils/date";
 
 
-export default function UsersList({ users, page, pages, count, onChange }) {
-  return (
-    <div>
-      <table>
-        <thead>
-          <tr colSpan={6}>
-            <th>
-              <div className="tableTtitle">Users</div>
-            </th>
-          </tr>
-          <tr>
-            <th>id</th>
-            <th>name</th>
-            <th>Last name</th>
-            <th>Email</th>
-            <th>Birthday</th>
-            
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr data-user-id={user.id} onClick={onChange} key={user.id}>
-              <td>{user.id}</td>
-              <td>{user.nombre}</td>
-              <td>{user.apellidos}</td>
-              <td>{user.email}</td>
-              <td>{dateFormat(user.fecha_nacimiento)}</td>
-              <td>{user?.usuario ? "YES" : "NO"}</td>
-            </tr>
-          ))}
-        </tbody>
-        <tfoot>
-          <tr>
-            <td colSpan={6}>
-              <TablePagination page={page} pages ={pages} limit={users.length} count={count} onChange={onChange}/>
-            </td>
-          </tr>
-        </tfoot>
-      </table>
-    </div>
-  );
-}
+
+// export default function UsersList({ users, page, pages, count, onChange }) {
+//   return (
+//     <div>
+//       <table>
+//         <thead>
+//           <tr colSpan={6}>
+//             <th>
+//               <div className="tableTtitle">Users</div>
+//             </th>
+//           </tr>
+//           <tr>
+//             {/* <th>id</th>
+//             <th>name</th>
+//             <th>Last name</th>
+//             <th>Emailddddd</th>
+//             <th>Birthday</th>
+//              */}
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {users.map((user) => (
+//             <tr data-user-id={user.id} onClick={onChange} key={user.id}>
+//               <td>{user.id}</td>
+//               <td>{user.nombre}</td>
+//               <td>{user.apellidos}</td>
+//               <td>{user.email}</td>
+//               <td>{dateFormat(user.fecha_nacimiento)}</td>
+//               <td>{user?.usuario ? "YES" : "NO"}</td>
+//             </tr>
+//           ))}
+//         </tbody>
+//         <tfoot>
+//           <tr>
+//             <td colSpan={6}>
+//               <TablePagination page={page} pages ={pages} limit={users.length} count={count} onChange={onChange}/>
+//             </td>
+//           </tr>
+//         </tfoot>
+//       </table>
+//     </div>
+//   );
+// }
--- src/containers/contact/Contact.jsx
+++ src/containers/contact/Contact.jsx
@@ -2,6 +2,28 @@ import React from 'react'
 
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <div>Contact
+       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic accusamus, earum, et veniam soluta quae suscipit aliquam eum perspiciatis laudantium ab amet! Nesciunt animi sequi ducimus! Reiciendis ipsa voluptas accusamus?
+    Itaque, pariatur facilis, quidem, quod voluptatibus porro explicabo repellat ratione sit animi impedit! Dignissimos molestiae odio libero neque corporis, fugiat distinctio suscipit laudantium eos possimus nobis, quia cum explicabo beatae.
+    Error doloremque, placeat officiis beatae fugiat officia aperiam dicta atque, mollitia ducimus et quam voluptate. Voluptas, libero! Ipsa, et distinctio voluptate illo deleniti voluptatibus incidunt impedit magnam quas dignissimos quaerat!
+    Dolorum ipsum adipisci suo, eveniet enim quibusdam exercitationem. Ad, recusandae aspernatur autem veritatis iure, distinctio animi harum aliquam vel esse reiciendis praesentium nam.
+    Debitis animi id ea facilis voluptas. Vel necessitatibus maxime cumque magnam voluptatibus dolorum quaerat minus tempora sed consequuntur. Nesciunt debitis laboriosam tempora neque tempore magnam quo eaque aspernatur temporibus tenetur?
+    Enim nam quas possimus recusandae sit qui minus officiis commodi saepe quia. Asperiores atque porro, dolore cumque dicta consequatur, velit cupiditate expedita ad soluta ipsum magnam optio ea culpa qui!
+    Tempore dolores provident pariatur, quisquam soluta nostrum molestias autem, in, nobis dignissimos excepturi ducimus omnis consectetur eum odio iste vitae ad mollitia tempora! Consectetur quisquam ratione consequuntur dignissimos quia possimus!
+    Neque explicabo animi nisi rem, facilis rerum sit inventore voluptate dolorum vel odit nobis expedita excepturi enim quae commodi dolorem perspiciatis. Ab iure tempora placeat veniam ad neque perspiciatis doloremque.
+    Dicta nisi quos asperiores atque consectetur ipsam facilis voluptatum illum eos vero cum placeat, saepe labore eum vitae non ratione quisquam ullam iusto repellendus error dolore omnis maiores. Architecto, ea.
+    Vero placeat debitis sapiente voluptates inventore voluptate beatae necessitatibus quos deserunt totam? Distinctio, fuga enim itaque totam nulla nobis odio harum nostrum dolore in illo omnis ea nesciunt provident. Enim.
+    Voluptatem ratione dignissimos ad numquam repellat! Soluta repudiandae magnam expedita, ullam quis in aperiam harum iure dolores dolore, sint deserunt laboriosam. Eveniet, unde nemo! Delectus corporis nam eius reiciendis iste!
+    Et, ratione animi consectetur cumque eligendi tempore minus sequi doloribus corrupti saepe! Harum porro id vitae ea, maiores quia repellat rerum numquam laborum labore illum in eius accusantium earum voluptate.
+    Ad a eius exercitationem fugit aspernatur vero modi nobis dolores, libero illum, nulla, possimus animi eligendi asperiores obcaecati quisquam iusto neque! Quas maxime qui dolorum. Dolor ratione ipsa ab a.
+    Ipsa consequatur dicta autem blanditiis in dolorem ad explicabo, commodi cumque dolor exercitationem eligendi accusantium unde, provident harum, perspiciatis quam ab eius sit! Perspiciatis labore atque, delectus nam animi quaerat.
+    Optio laudantium labore sed distinctio ratione perspiciatis sequi, architecto velit tenetur explicabo voluptatem iusto molestias expedita commodi quia odio dolorum eius nulla deleniti consectetur ad quis. Voluptas aliquid ea possimus!
+    Laborum sunt maxime officiis beatae eius culpa suscipit ab alias, quia amet ex error necessitatibus quas natus? Ipsam debitis numquam voluptate, quam sunt iste, dignissimos aut pariatur cupiditate facere quidem.
+    Tempore alias fugiat ducimus libero ullam harum unde esse, sit commodi vel exercitationem impedit provident recusandae mollitia consequuntur est quam consequatur velit maiores deleniti illum at. Vitae vero aspernatur natus.
+    Unde atque necessitatibus quia! Sit culpa vitae aliquam? Aliquam accusantium, necessitatibus ut molestias eligendi eveniet rerum voluptates, doloribus iste, aperiam temporibus quidem atque ipsa explicabo autem facere debitis. Quod, odio?
+    Laudantium autem facilis quaerat harum! Necessitatibus nisi vel nesciunt amet deleniti, eligendi, saepe obcaecati suscipit tempora voluptates sint quam velit molestiae earum temporibus in facere eum quisquam veniam, dolorum dicta.
+    Enim tempore necessitatibus, facere blanditiis vero facilis. Esse unde autem nam praesentium repellendus, quam sunt eum temporibus doloremque iste sed tempore commodi dicta, ipsum nemo accusamus distinctio doloribus neque placeat.
+    Aspernatur est facilis temp</p>
+    </div>
   )
 }
