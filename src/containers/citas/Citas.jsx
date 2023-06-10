809644ac318a3c864424dd041445d76d64352fda
César David Parada Castillo <cesard.0925@gmail.com>
2023-06-10 15:35:52 +0200

    chore: arreglo menor

--- src/containers/citas/Citas.jsx
+++ src/containers/citas/Citas.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import citaService from "../../_services/citaService";
 import { useSelector } from "react-redux";
